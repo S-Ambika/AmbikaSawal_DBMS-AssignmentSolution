f73ee21362a3d230fdd6a67ed9d36d20ee51ace6
S-Ambika <48045863+S-Ambika@users.noreply.github.com>
2021-11-13 18:23:14 +0530

    Update AmbikaSawal_DBMS_AssignmentQuery.sql

--- AmbikaSawal_DBMS_AssignmentQuery.sql
+++ AmbikaSawal_DBMS_AssignmentQuery.sql
@@ -7,8 +7,8 @@ use TravelOnTheGo;
 create table PASSENGER (Passenger_name varchar (50),
                         Category varchar (50),
                         Gender varchar (2),
-						Boarding_City varchar (30),
-						Destination_City varchar (30),
+			Boarding_City varchar (30),
+			Destination_City varchar (30),
                         Distance int ,
                         Bus_Type varchar (30)
                         );
@@ -21,7 +21,7 @@ create table PRICE (Bus_Type varchar (30),
 2. Inserting values inside Tables
 */
 insert into PASSENGER values('Sejal' ,'AC', 'F', 'Bengaluru', 'Chennai', 350, 'Sleeper'),('Anmol',' Non-AC', 'M', 'Mumbai', 'Hyderabad', 700,'Sitting'),('Pallavi', 'AC', 'F', 'Panaji', 'Bengaluru', 600 ,'Sleeper'),
-							('Khusboo', 'AC', 'F', 'Chennai', 'Mumbai', 1500, 'Sleeper'),('Udit', 'Non-AC', 'M', 'Trivandrum', 'panaji', 1000, 'Sleeper'),('Ankur', 'AC', 'M' ,'Nagpur', 'Hyderabad', 500 ,'Sitting'),
+			    ('Khusboo', 'AC', 'F', 'Chennai', 'Mumbai', 1500, 'Sleeper'),('Udit', 'Non-AC', 'M', 'Trivandrum', 'panaji', 1000, 'Sleeper'),('Ankur', 'AC', 'M' ,'Nagpur', 'Hyderabad', 500 ,'Sitting'),
                             ('Hemant', 'Non-AC' ,'M' ,'panaji' ,'Mumbai', 700, 'Sleeper'),('Manish', 'Non-AC', 'M', 'Hyderabad', 'Bengaluru', 500, 'Sitting'),('Piyush', 'AC', 'M', 'Pune', 'Nagpur', 700 ,'Sitting');
 insert into PRICE values('Sleeper', 350, 770),('Sleeper' ,500 ,1100),('Sleeper', 600, 1320),('Sleeper', 700, 1540),('Sleeper', 1000, 2200),('Sleeper' ,1200, 2640),
                          ('Sleeper', 350 ,434),('Sitting', 500 ,620),('Sitting', 500 ,620),('Sitting', 600, 744),('Sitting', 700, 868),('Sitting', 1000, 1240),('Sitting', 1200, 1488),
